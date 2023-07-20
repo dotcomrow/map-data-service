d7737ebc9a309935cba8c61ee774d03ecc72580b
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-07-19 22:57:39 -0400

    test update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/main/java/com/cloudrun/microservicetemplate/config/WebSecurityConfig.java
+++ src/main/java/com/cloudrun/microservicetemplate/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.cloudrun.microservicetemplate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,11 +14,11 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.oauth2Login((authz) -> authz
-                .successHandler((request, response, authentication) -> {
-                    response.sendRedirect("/map-data");
-                }));
-        // @formatter:on
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+            authorizeHttpRequests
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(Customizer.withDefaults());
         
         return http.build();
     }
